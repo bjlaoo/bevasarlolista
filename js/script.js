b8ea1cd725093564f7d6731d4f574931c9dd9892
bjlaoo <92033939+bjlaoo@users.noreply.github.com>
2024-01-15 21:25:59 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 //read json
-
 xmlhttp = new XMLHttpRequest();
 xmlhttp.onload = function () {
     let x = JSON.parse(this.responseText);
@@ -28,12 +27,23 @@ xmlhttp.onload = function () {
     document.querySelector(".btn-del-list").addEventListener("click", function () {
         for (let i = 0; i < toDelList.length; i++) {
             lists[toDelList[i]] = null;
+            selectedList=null;            
         }
+        
         for (let i = 0; i < lists.length; i++) {
             if (lists[i] != null) {
                 selectedList = lists[i];
             }
-
+            
+        }
+        if(selectedList===null){
+            selectedList=new elementlist("Üres lista");
+            lists.push(selectedList);
+            for (let i = 0; i < lists.length; i++) {
+                if (lists[i] != null) {
+                    selectedList = lists[i];
+                }
+            }
         }
         write(selectedList);
     })
@@ -102,20 +112,7 @@ function write(selectedList) {
             });
         })(i);
     }
-    let todelelements = document.querySelectorAll(".store");
-    for (let i = 0; i < todelelements.length; i++) {
-        (function (index) {
-            todelelements[index].addEventListener("click", function (event) {
-                if (!event.target.classList.contains("store-edit")) {
-                    if (todelelements[index].classList.contains("selected-todel-element")) {
-                        todelelements[index].classList.remove("selected-todel-element");
-                    } else {
-                        todelelements[index].classList.add("selected-todel-element");
-                    }
-                }
-            });
-        })(i);
-    }
+
 
 
     let tableNumber = Math.ceil(storeslist.length / 10);
@@ -131,19 +128,52 @@ function write(selectedList) {
             }
         }
     }
+    let todelelements = document.querySelectorAll(".store");
+    for (let i = 0; i < todelelements.length; i++) {
+        (function(index) {
+            todelelements[index].addEventListener("click", function(event) {
+                if (!event.target.classList.contains("store-edit")) {
+                    if (todelelements[index].classList.contains("selected-todel-element")) {
+                        todelelements[index].classList.remove("selected-todel-element");
+                    } else {
+                        todelelements[index].classList.add("selected-todel-element");
+                    }
+                }
+            });
+        })(i);
+    }
+        //page (storepage)
+    let pages = document.querySelectorAll(".page");
+    for (let i = 1; i < pages.length; i++) {
+        pages[i].style.display = "none";
+    }
+    let page = 0;
+    document.querySelector(".btn-left").addEventListener("click", function left() {
+        if (page > 0) {
+            document.querySelector(".page-" + page).style.display = "none";
+            page--;
+            document.querySelector(".page-" + page).style.display = "block";
+        }
+    });
+    document.querySelector(".btn-right").addEventListener("click", function right() {
+        if (page < pages.length-1) {
+            document.querySelector(".page-" + page).style.display = "none";
+            page++;
+            document.querySelector(".page-" + page).style.display = "block";
+        }
+    });
     openBtns = document.querySelectorAll(".btn-open");
     for (let i = 0; i < openBtns.length; i++) {
         openBtns[i].addEventListener("click", function () {
             n = openBtns[i].classList[2];
             selectedList = lists[n];
             write(selectedList);
-            
+
             load(document.querySelector(".starting-container"));
             console.log(selectedList);
         });
     }
     //list rename
-    
     let listName = document.querySelector(".list-name");
     listName.innerHTML = selectedList.getName() + "<sup><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"10\" height=\"10\" fill=\"currentColor\" class=\"bi bi-pencil-square\" viewBox=\"0 0 16 16\"><path d=\"M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z\"/><path fill-rule=\"evenodd\" d=\"M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z\"/></svg></sup>";
     listName.addEventListener("click", function () {
@@ -152,10 +182,10 @@ function write(selectedList) {
         listName.innerHTML = '<input type="text" class="form-control" placeholder="' + selectedList.getName() + '">';
         let inputElement = listName.querySelector('input');
         inputElement.focus();
-    
+
         // Remove existing "keydown" event listener
         $(document).off("keydown");
-    
+
         // Add new "keydown" event listener
         $(document).on("keydown", function (event) {
             switch (event.keyCode) {
@@ -169,48 +199,31 @@ function write(selectedList) {
             }
         });
     });
-    
+
 }
 let lists = [];
 let storeslist = [];
 
 
-//page (storepage)
-let pages = document.querySelectorAll(".page");
-for (let i = 1; i < pages.length; i++) {
-    pages[i].style.display = "none";
-}
-let page = 1;
-document.querySelector(".btn-left").addEventListener("click", function left() {
-    if (page > 1) {
-        document.querySelector(".page-" + page).style.display = "none";
-        page--;
-        document.querySelector(".page-" + page).style.display = "block";
-    }
-});
-document.querySelector(".btn-right").addEventListener("click", function right() {
-    if (page < 3) {
-        document.querySelector(".page-" + page).style.display = "none";
-        page++;
-        document.querySelector(".page-" + page).style.display = "block";
-    }
-});
 
-function Torles() {
+document.querySelector(".del-button-list").addEventListener("click",function () {
     let selectedLists = document.querySelectorAll(".selected-list");
     let p = document.querySelector(".del-lists");
     p.innerHTML = "";
     for (let i = 0; i < selectedLists.length; i++) {
         for (let j = 0; j < lists.length; j++) {
-            if (selectedLists[i].classList[2] == lists[j].getId()) {
-                if (i != selectedLists.length - 1) {
-                    p.innerHTML += lists[j].getName() + ", ";
-                }
-                else {
-                    p.innerHTML += lists[j].getName();
+            if(lists[j]!=null){
+                if (selectedLists[i].classList[2] == lists[j].getId()) {
+                    if (i != selectedLists.length - 1) {
+                        p.innerHTML += lists[j].getName() + ", ";
+                    }
+                    else {
+                        p.innerHTML += lists[j].getName();
+                    }
                 }
             }
-        }
 
+        }
     }
 }
+)
