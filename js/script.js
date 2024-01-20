ac01373b53874e2976cf46851ed2fe00b487abed
KalauzBalazs <balazs.kalauz@gmail.com>
2024-01-20 14:22:11 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-//read json"
-let selectedList;
+//read json
+var selectedList;
 let index;
 xmlhttp = new XMLHttpRequest();
 xmlhttp.onload = function () {
@@ -300,12 +300,13 @@ function write() {
 
 
     //list rename
-    let listName = document.querySelector(".list-name");
-    listName.innerHTML = selectedList.getName() + "<sup><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"10\" height=\"10\" fill=\"currentColor\" class=\"bi bi-pencil-square\" viewBox=\"0 0 16 16\"><path d=\"M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z\"/><path fill-rule=\"evenodd\" d=\"M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z\"/></svg></sup>";
-    listName.addEventListener("click", function () {
+    let listName = document.querySelectorAll(".list-name");
+    for(let i=0;i<listName.length;i++){
+        listName[i].innerHTML = selectedList.getName() + "<sup><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"10\" height=\"10\" fill=\"currentColor\" class=\"bi bi-pencil-square\" viewBox=\"0 0 16 16\"><path d=\"M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z\"/><path fill-rule=\"evenodd\" d=\"M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z\"/></svg></sup>";
+        listName[i].addEventListener("click", function () {
         let originalText = selectedList.getName();
-        listName.innerHTML = '<input type="text" class="form-control" placeholder="' + selectedList.getName() + '">';
-        let inputElement = listName.querySelector('input');
+        listName[i].innerHTML = '<input type="text" class="form-control" placeholder="' + selectedList.getName() + '">';
+        let inputElement = listName[i].querySelector('input');
         inputElement.focus();
 
         // Remove existing "keydown" event listener
@@ -324,6 +325,7 @@ function write() {
             }
         });
     });
+}
     //edit element
     editElements = document.querySelectorAll(".btn-set-element");
     for (let i = 0; i < editElements.length; i++) {
